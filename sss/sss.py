825ee6b1d8d7baaa74f5c94912dd2256dac7c026
nghiatc <congnghia0609@gmail.com>
2020-03-10 11:48:48 +0700

    update code

--- sss/sss.py
+++ sss/sss.py
@@ -45,7 +45,7 @@ def modinv(a, m):
 def to_base64(number):
     numbyte = number.to_bytes(32, 'big')
     b64data = b64encode(numbyte)
-    return b64data
+    return b64data.decode('ascii')
 
 
 # Returns the number base64 in base 10 Int representation; note: this is
@@ -138,6 +138,76 @@ def in_numbers(numbers, value):
     return False
 
 
+# Returns a new array of secret shares (encoding x,y pairs as Base64 or Hex strings)
+# created by Shamir's Secret Sharing Algorithm requiring a minimum number of
+# share to recreate, of length shares, from the input secret raw as a string.
+def create(minimum, shares, secret, is_base64):
+    result = []
+    if minimum > shares:
+        raise Exception('cannot require more shares then existing')
+    # Convert the secrets to its respective 256-bit Int representation.
+    secrets = split_secret_to_int(secret)
+
+    # List of currently used numbers in the polynomial
+    numbers = [0]
+
+    # Create the polynomial of degree (minimum - 1); that is, the highest
+    # order term is (minimum-1), though as there is a constant term with
+    # order 0, there are (minimum) number of coefficients.
+    #
+    # However, the polynomial object is a 2d array, because we are constructing
+    # a different polynomial for each part of the secrets.
+    #
+    # polynomial[parts][minimum]
+    polynomial = [[0] * minimum] * len(secrets)
+    print(polynomial)
+    for i in range(len(polynomial)):
+        polynomial[i][0] = secrets[i]
+        for j in range(len(polynomial[i])):
+            if j > 0:
+                # Each coefficient should be unique
+                number = random_number()
+                while in_numbers(numbers, number):
+                    number = random_number()
+                numbers.append(number)
+
+                polynomial[i][j] = number
+
+    # Create the points object; this holds the (x, y) points of each share.
+    # Again, because secrets is an array, each share could have multiple parts
+    # over which we are computing Shamir's Algorithm. The last dimension is
+    # always two, as it is storing an x, y pair of points.
+    #
+    # Note: this array is technically unnecessary due to creating result
+    # in the inner loop. Can disappear later if desired.
+    #
+    # points[shares][parts][2]
+    points = [[[0] * 2] * len(secrets)] * shares
+    print(points)
+    for i in range(len(points)):
+        s = ""
+        for j in range(len(points[i])):
+            # generate a new x-coordinate.
+            number = random_number()
+            while in_numbers(numbers, number):
+                number = random_number()
+            numbers.append(number)
+
+            # and evaluate the polynomial at that point.
+            points[i][j][0] = number
+            points[i][j][1] = evaluate_polynomial(polynomial[j], number)
+
+            # add it to results.
+            if is_base64:
+                s += to_base64(points[i][j][0])
+                s += to_base64(points[i][j][1])
+            else:
+                s += to_hex(points[i][j][0])
+                s += to_hex(points[i][j][1])
+        result.append(s)
+    return result
+
+
 if __name__ == '__main__':
     # # 1. random_number
     # for i in range(100):
@@ -159,7 +229,7 @@ if __name__ == '__main__':
     # number = 67356225285819719212258382314594931188352598651646313425411610888829358649431
     # print(number)
     # b64data = to_base64(number)
-    # print(b64data)  # b'lOpFwywpCeVAcK0/LOKG+YtW71xyj1bX06CcW7VZMFc='
+    # print(b64data)  # lOpFwywpCeVAcK0/LOKG+YtW71xyj1bX06CcW7VZMFc=
     # hexdata = to_hex(number)
     # print(len(hexdata))  # 64
     # print(hexdata)  # 94ea45c32c2909e54070ad3f2ce286f98b56ef5c728f56d7d3a09c5bb5593057
@@ -169,11 +239,18 @@ if __name__ == '__main__':
     # numhexdecode = from_hex(hexdata)
     # print(numhexdecode)
 
-    # 4. split & merge
+    # # 4. split & merge
+    # s = "nghiatcxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    # print(s)
+    # arr = split_secret_to_int(s)
+    # print(arr)
+    # print(in_numbers(arr, 49937119214509114343548691117920141602615245118674498473442528546336026425464))
+    # rs = merge_int_to_string(arr)
+    # print(rs)
+
+    # 5. create
+    # creates a set of shares
     s = "nghiatcxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    print(s)
-    arr = split_secret_to_int(s)
+    arr = create(3, 6, s, False)
     print(arr)
-    print(in_numbers(arr, 49937119214509114343548691117920141602615245118674498473442528546336026425464))
-    rs = merge_int_to_string(arr)
-    print(rs)
+
